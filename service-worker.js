dd7b1501598e4fa1abfc9f168f085b8610c64b58
borshevikas11 <nasty.panukova2@gmail.com>
2025-07-22 22:07:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,21 @@
-const CACHE_NAME = 'sportlife-cache-v1';
+const CACHE_NAME = 'sportlife-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/nutrition.html',
-  '/sports.html',
   '/style.css',
-  '/images/about.jpg',
-  '/images/food1.jpg',
-  '/images/food2.jpg',
-  '/images/food3.jpg',
-  '/images/food4.jpg',
-  '/images/hero-bg.jpg'
+  '/images/hero-bg.jpg',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
+      .then(cache => cache.addAll(urlsToCache))
+);
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
+      .then(response => response || fetch(event.request))
+);
